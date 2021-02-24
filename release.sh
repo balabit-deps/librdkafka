d1f701eb3120d390e874477272eb81d0ef8469ad
Kokan <kokaipeter@gmail.com>
2021-02-24 08:53:19 +0100

    gitlab-ci: fix the escaping of -rpath,
    The flag can be checked via: readelf -d ./artifacts/=install/opt/syslog-ng/lib/libsasl2.so|grep -i FLAGS
    
    Result should have ORIGIN:
    0x000000006ffffffb (FLAGS_1)            Flags: ORIGIN
    
    Signed-off-by: Kokan <kokaipeter@gmail.com>

--- release.sh
+++ release.sh
@@ -6,8 +6,8 @@ ls /opt/syslog-ng/lib/
 
 export PKG_CONFIG_PATH="/opt/syslog-ng/lib/pkgconfig:$PKG_CONFIG_PATH"
 export LIBRARY_PATH="$LIBRARY_PATH:/opt/syslog-ng/lib"
-export CFLAGS='-O2 -isystem /opt/syslog-ng/include -Wl,-z,origin,-rpath,\$$ORIGIN/../lib'
+export CFLAGS='-O2 -isystem /opt/syslog-ng/include'
 
-./configure --prefix=/opt/syslog-ng/ 
+./configure --prefix=/opt/syslog-ng/ --LDFLAGS='-Wl,-z,origin,-rpath,\$$ORIGIN/../lib'
 make all
 make DESTDIR=`pwd`/artifacts/=install/ install
